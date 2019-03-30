279100d1957d53157aef096a9449b24db65281af
Thierry de Pauw <thierry@thinkinglabs.io>
2019-03-30 18:07:04 +0100

    add success message 'Passed!' to assert.equal()

--- test/Dates-test.js
+++ test/Dates-test.js
@@ -4,55 +4,55 @@ QUnit.module('Dates', function() {
   QUnit.module('daysInMonth', function() {
 
     QUnit.test('February 2016 is 29', function( assert ) {
-      assert.equal(daysInMonth(2016, 1), 29);
+      assert.equal(daysInMonth(2016, 1), 29, 'Passed!');
     });
 
     QUnit.test('January 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 0), 31);
+      assert.equal(daysInMonth(2019, 0), 31, 'Passed!');
     });
   
     QUnit.test('February 2019 is 28', function( assert ) {
-      assert.equal(daysInMonth(2019, 1), 28);
+      assert.equal(daysInMonth(2019, 1), 28, 'Passed!');
     });
 
     QUnit.test('March 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 2), 31);
+      assert.equal(daysInMonth(2019, 2), 31, 'Passed!');
     });
 
     QUnit.test('April 2019 is 30', function( assert ) {
-      assert.equal(daysInMonth(2019, 3), 30);
+      assert.equal(daysInMonth(2019, 3), 30, 'Passed!');
     });
 
     QUnit.test('May 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 4), 31);
+      assert.equal(daysInMonth(2019, 4), 31, 'Passed!');
     });
 
     QUnit.test('June 2019 is 30', function( assert ) {
-      assert.equal(daysInMonth(2019, 5), 30);
+      assert.equal(daysInMonth(2019, 5), 30, 'Passed!');
     });
 
     QUnit.test('July 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 6), 31);
+      assert.equal(daysInMonth(2019, 6), 31, 'Passed!');
     });
 
     QUnit.test('August 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 7), 31);
+      assert.equal(daysInMonth(2019, 7), 31, 'Passed!');
     });
 
     QUnit.test('September 2019 is 30', function( assert ) {
-      assert.equal(daysInMonth(2019, 8), 30);
+      assert.equal(daysInMonth(2019, 8), 30, 'Passed!');
     });
 
     QUnit.test('October 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 9), 31);
+      assert.equal(daysInMonth(2019, 9), 31, 'Passed!');
     });
 
     QUnit.test('November 2019 is 30', function( assert ) {
-      assert.equal(daysInMonth(2019, 10), 30);
+      assert.equal(daysInMonth(2019, 10), 30, 'Passed!');
     });
 
     QUnit.test('December 2019 is 31', function( assert ) {
-      assert.equal(daysInMonth(2019, 11), 31);
+      assert.equal(daysInMonth(2019, 11), 31, 'Passed!');
     });
   });
 
@@ -65,11 +65,11 @@ QUnit.module('Dates', function() {
   QUnit.module('formatISODate', function() {
 
     QUnit.test('2019-03-17', function( assert ) {
-      assert.equal(formatISODate(new Date(2019, 2, 17)), '2019-03-17');
+      assert.equal(formatISODate(new Date(2019, 2, 17)), '2019-03-17', 'Passed!');
     });
   
     QUnit.test('2019-10-01', function( assert ) {
-      assert.equal(formatISODate(new Date(2019, 9, 1)), '2019-10-01');
+      assert.equal(formatISODate(new Date(2019, 9, 1)), '2019-10-01', 'Passed!');
     });
 
   });
@@ -77,31 +77,31 @@ QUnit.module('Dates', function() {
   QUnit.module('formatYYYYMM', function() {
 
     QUnit.test('2019-03-17', function( assert ) {
-      assert.equal(formatYYYYMM(new Date(2019, 2, 17)), '201903');
+      assert.equal(formatYYYYMM(new Date(2019, 2, 17)), '201903', 'Passed!');
     });
 
   });
 
   QUnit.test('firstDayOfMonth', function ( assert ) {
-    assert.deepEqual(firstDayOfMonth(new Date(2019, 1, 27)), new Date(2019, 1, 1));
+    assert.deepEqual(firstDayOfMonth(new Date(2019, 1, 27)), new Date(2019, 1, 1), 'Passed!');
   });
 
   QUnit.module('lastDayOfMonth', function() {
 
     QUnit.test('February 2016', function( assert ) {
-      assert.deepEqual(lastDayOfMonth(new Date(2016, 1, 15)), new Date(2016, 1, 29));
+      assert.deepEqual(lastDayOfMonth(new Date(2016, 1, 15)), new Date(2016, 1, 29), 'Passed!');
     });
 
     QUnit.test('January 2019', function( assert ) {
-      assert.deepEqual(lastDayOfMonth(new Date(2019, 0, 11)), new Date(2019, 0, 31));
+      assert.deepEqual(lastDayOfMonth(new Date(2019, 0, 11)), new Date(2019, 0, 31), 'Passed!');
     });
   
     QUnit.test('February 2019 is 28', function( assert ) {
-      assert.deepEqual(lastDayOfMonth(new Date(2019, 1, 21)), new Date(2019, 1, 28));
+      assert.deepEqual(lastDayOfMonth(new Date(2019, 1, 21)), new Date(2019, 1, 28), 'Passed!');
     });
 
     QUnit.test('March 2019 is 31', function( assert ) {
-      assert.deepEqual(lastDayOfMonth(new Date(2019, 2, 3)), new Date(2019, 2, 31));
+      assert.deepEqual(lastDayOfMonth(new Date(2019, 2, 3)), new Date(2019, 2, 31), 'Passed!');
     });
 
   });
@@ -111,11 +111,11 @@ QUnit.module('Dates', function() {
 QUnit.module('String', function() {
 
   QUnit.test('padStart - 5 -> 05', function( assert ) {
-    assert.equal(padStart(5, 2), '05');
+    assert.equal(padStart(5, 2), '05', 'Passed!');
   });
 
   QUnit.test('padStart - 31 -> 31', function( assert ) {
-    assert.equal(padStart(31, 2), '31');
+    assert.equal(padStart(31, 2), '31', 'Passed!');
   });
 
 });
